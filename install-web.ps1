4bdc4137fe9e06cd74e30a25dbaf1c35023b8898
Banana-Toast-GH <hghghhghghghg679@gmail.com>
2026-02-20 05:29:39 -0600

    Fix installer cache busting

--- install-web.ps1
+++ install-web.ps1
@@ -1,7 +1,8 @@
 # BPE Web Installer Bootstrap
 # Run: irm https://raw.githubusercontent.com/Banana-Toast-GH/BananaProjectEditor/main/install-web.ps1 | iex
 
-$url = "https://raw.githubusercontent.com/Banana-Toast-GH/BananaProjectEditor/main/Install-BPE.ps1"
-$tmp = "$env:TEMP\Install-BPE.ps1"
+$url = "https://raw.githubusercontent.com/Banana-Toast-GH/BananaProjectEditor/main/Install-BPE.ps1?t=$([DateTimeOffset]::UtcNow.ToUnixTimeSeconds())"
+$tmp = "$env:TEMP\Install-BPE-$(Get-Random).ps1"
 (New-Object System.Net.WebClient).DownloadFile($url, $tmp)
 powershell.exe -ExecutionPolicy Bypass -File $tmp
+Remove-Item $tmp -Force -ErrorAction SilentlyContinue
